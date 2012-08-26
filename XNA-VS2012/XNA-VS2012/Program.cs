653c61f99d1de64088cf82288b7a861aa1c1c574
Michael Novikov <sigod.mail@gmail.com>
2012-08-26 16:33:13 +0300

    Added project and .gitignore.

---
+++ XNA-VS2012/XNA-VS2012/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace XNA_VS2012
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			using (Game1 game = new Game1())
+			{
+				game.Run();
+			}
+		}
+	}
+}
